8fbfd8fc013b5a2ceeb2e23793b515c847c9dfdd
Pratyush Shah <205055816+HarmoniousJoker@users.noreply.github.com>
2025-04-02 18:01:48 -0400

    Implemented Elend Art

--- src/joker/scadrial/elend.lua
+++ src/joker/scadrial/elend.lua
@@ -1,8 +1,8 @@
 --Elend: 4 in 16 chance of gaining +2 mult per scored face card
 SMODS.Joker {
 	key = 'elend',
-	--atlas = 'scadrial',
-	pos = { x = 9, y = 9 },
+	atlas = 'scadrial',
+	pos = { x = 4, y = 0 },
 	rarity = 'csmr_preserver',
 	cost = 4,
 	unlocked = true,
