6814097a8def4b74448011c97d35ced3d5d633d3
Pratyush Shah <205055816+HarmoniousJoker@users.noreply.github.com>
2025-04-16 03:15:30 -0400

    Fixed Edgard Bug

--- src/joker/scadrial/edgard.lua
+++ src/joker/scadrial/edgard.lua
@@ -9,7 +9,21 @@ SMODS.Joker {
 	discovered = true,
 	calculate = function(self, card, context)
         if G.GAME.blind.in_blind and context.selling_card then
-            ease_chips(0.75 * G.GAME.blind.chips + G.GAME.chips)
+			if context.card.config.center.key == 'j_csmr_edgard' then
+            	ease_chips(0.75 * G.GAME.blind.chips + G.GAME.chips)
+			end
+			G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+				delay = 0.5,
+                func = function()
+					if G.GAME.chips >= G.GAME.blind.chips then					
+						G.STATE = G.STATES.HAND_PLAYED
+						G.STATE_COMPLETE = true
+						end_round()
+					end
+					return true
+				end
+            }))
         end
 	end
 }
