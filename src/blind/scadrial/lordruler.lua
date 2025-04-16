36b1305023d7c269f7fc4be8f46e2b04c602f64f
Pratyush Shah <205055816+HarmoniousJoker@users.noreply.github.com>
2025-04-16 17:08:35 -0400

    Fixed Lord Ruler appearing in earlier Antes

--- src/blind/scadrial/lordruler.lua
+++ src/blind/scadrial/lordruler.lua
@@ -6,21 +6,6 @@ SMODS.Blind{
     --atlas = 'scadrial',
     boss = {min = 4, max = 10},
     pos = { x = 0, y = 30},
-    loc_vars = function(self)
-    end,
-    collection_loc_vars = function(self)
-    end,
-    set_blind = function(self)
-    end,
-    drawn_to_hand = function(self)
-    end,
-    in_pool = function(self)
-        return true
-    end,
-    disable = function(self)
-    end,
-    defeat = function(self)
-    end,
     press_play = function(self)
         if not G.GAME["LordRulerMultiplier"] then
             G.GAME["LordRulerMultiplier"] = 1
